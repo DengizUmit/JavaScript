7b14fd3887704552be1cc1adb4b9e203683a0c53
Ümit Dengiz <48486462+dengizUmit@users.noreply.github.com>
2021-03-30 00:18:25 +0300

    JS Variables
    JavaScript variables are containers for storing data values.

---
+++ degiskenler.js
@@ -0,0 +1,9 @@
+var ad = prompt("Adınız : ");
+var ogrNo = prompt("Numaranız : ");
+var cinsiyet = prompt("Cinsiyet"); // Erkek: true Kadın: false
+var ders = prompt("Ders Adi : ");
+var not1 = prompt("Not 1 : ");
+var not1 = prompt("Not 2 : ");
+var ortalama = (Number(Not1) + Number(Not2))/2;
+
+console.log(ad+" "+ogrNo+" "+cinsiyet+" "+ders+" "+ortalama);
